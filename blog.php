30678ffe16f1877346a0edb25d11c5db10484c92
redaProg-wq <rdamekouar@gmail.com>
2024-06-22 10:01:17 +0200

    Version1pasfinie

---
+++ blog.php
@@ -0,0 +1,83 @@
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Golos+Text:wght@400..900&family=Mukta:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+
+    <!--Google fonts-->
+
+    <!--icons-->
+    <script
+      src="https://kit.fontawesome.com/642ea75ff0.js"
+      crossorigin="anonymous"
+    ></script>
+    <!--icons-->
+
+    <!-- JavaScipt-->
+    <script type="module" src="js/index.js"></script>
+    <!--Javascript-->
+    <!--Bootstrap
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+    Bootstrap -->
+    <!--local style-->
+    <link rel="stylesheet" href="css/style.css" />
+    <!--Local style-->
+
+    <title>Reda Hypnose</title>
+  </head>
+  <body>
+   
+  
+      
+      <!-- Modal + burger menu-->
+      <div class="modal-mobile">
+        <ul class="modal-navbar">
+          <li><a href="./index.html">A PROPOS</a></li>
+          <li><a href="./services.html">SERVICES</a></li>
+          <li><a href="./temoignages.html">TEMOIGNAGES</a></li>
+          <li><a href="./blog.php">BLOG</a></li>
+          <li><a href="#">CONTACTS</a></li>
+        </ul>
+      </div>
+
+      <div class="icon-menu"><i class="fa-solid fa-bars"></i></div>
+      <div class="header-valueP">
+      <div class="valueP">
+        <div class="valueP-container">
+          <h2 class="dancing">
+            Reda Romain Hypnose <br />
+            <span class="yellow"> Le Blog  </span>
+          </h2>
+        </div>
+      </div>
+    </div>
+    <!--fin de la div header -v value p-->
+
+
+    <footer>
+      <p class="pContacts">
+        Reda Romain Mekouar <br />
+        Saint-Etienne 42000 <br />
+        0606962667 <br />
+        rdamekouar@gmail.com <br />
+        <span class="underlineyellow"> <a href="#">Mentions Legales</a></span>
+      </p>
+    </footer>
+  </body>
+</html>
